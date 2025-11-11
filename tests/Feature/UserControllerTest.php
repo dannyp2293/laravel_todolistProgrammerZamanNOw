081b717204288bc3fdb8ce016218a7d32d793050
dannyp2293 <dannyparlinbutarbutar@gmail.com>
2025-11-11 21:23:44 +0700

    dirumah masih emnu login

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -2,15 +2,36 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
-  public function testLoginPage()
-  {
-$this->get('/login')
-->assertSeeText("Login");
-  }
+    public function testLoginPage(): void
+    {
+        $this->get('/login')
+            ->assertSeeText("Login");
+    }
+
+    public function testLoginSuccess(): void
+    {
+        $this->post('/login', [
+            "user" => "danny",
+            "password" => "rahasia"
+        ])->assertRedirect("/")
+          ->assertSessionHas("user", "danny");
+    }
+
+    public function testLoginValidationError(): void
+    {
+        $this->post("/login", [])
+            ->assertSeeText("User or password is required");
+    }
+
+    public function testLoginFailed(): void
+    {
+        $this->post('/login', [
+            "user" => "wrong",
+            "password" => "wrong"
+        ])->assertSeeText("User or password is wrong");
+    }
 }
