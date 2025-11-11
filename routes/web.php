ae7ac628af3372916aeb69fb067d262072849cc2
dannyp2293 <dannybutarbutar@gmail.com>
2025-11-11 15:51:09 +0700

    sampe menu login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::view('/template', 'template');
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/login', 'login');
+    Route::post('/login','doLogin');
+    Route::get('/logout','doLogout');
 });
